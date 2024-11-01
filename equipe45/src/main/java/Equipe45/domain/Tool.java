9ee45bd7349a1c2b415c0af003b85f2b0a93f4a6
WilliamBourque <william.bourque.1@ulaval.ca>
2024-11-01 17:27:31 -0400

    Tool class started

--- equipe45/src/main/java/Equipe45/domain/Tool.java
+++ equipe45/src/main/java/Equipe45/domain/Tool.java
@@ -13,7 +13,53 @@ public class Tool {
     private float cutWidth;
     private int positionCharger;
 
+
     public Tool() {
     }
-    
+
+    public Tool(String name, float cutWidth, int positionCharger) {
+        this.name = name;
+        this.cutWidth = cutWidth;
+        this.positionCharger = positionCharger;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getCutWidth() {
+        return cutWidth;
+    }
+
+    public void setCutWidth(float cutWidth) {
+        if (cutWidth < 0) {
+            throw new IllegalArgumentException("Cut width cannot be negative.");
+        }
+        this.cutWidth = cutWidth;
+    }
+
+    public int getPositionCharger() {
+        return positionCharger;
+    }
+
+    public void setPositionCharger(int positionCharger) {
+        if (positionCharger < 0 || positionCharger > 11) {
+            throw new IllegalArgumentException("Position in charger must be between 0 and 11.");
+        }
+        this.positionCharger = positionCharger;
+    }
+
+    @Override
+    public String toString() {
+        return "Tool{" +
+                "name='" + name + '\'' +
+                ", cutWidth=" + cutWidth +
+                ", positionCharger=" + positionCharger +
+                '}';
+    }
 }
