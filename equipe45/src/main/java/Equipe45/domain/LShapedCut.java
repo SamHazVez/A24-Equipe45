5320fdd243f6b40e73685cbedf0fed9bc5637a09
Will-G3 <WIGIN2@ulaval.ca>
2024-11-22 23:56:50 -0500

    Modif de parallel cut et ajout de straight cut

--- equipe45/src/main/java/Equipe45/domain/LShapedCut.java
+++ equipe45/src/main/java/Equipe45/domain/LShapedCut.java
@@ -12,90 +12,21 @@ import Equipe45.domain.Utils.ReferenceCoordinate;
  * @author mat18
  */
 public class LShapedCut extends IrregularCut {
-    private ParallelCut horizontalCut;
-    private ParallelCut verticalCut;
+    private StraightCUt horizontalCut;
+    private StraightCUt verticalCut;
 
     public LShapedCut(float depth, Tool tool, ReferenceCoordinate reference, Coordinate intersection) {
         super(depth, tool, reference, intersection);
-        
-        ReferenceCoordinate.CornerType type = reference.determineCornerType();
-        float distanceX;
-        float distanceY;
 
-        System.out.println("Origine Coupe Horizontal :" + reference.horizontalCut.getOrigin().getX() + ","+ reference.horizontalCut.getOrigin().getY());;
-        System.out.println("Destination Coupe Horizontal :" + reference.horizontalCut.getDestination().getX() + ","+ reference.horizontalCut.getDestination().getY());;
-
-        System.out.println("Origine Coupe Vertical :" + reference.verticalCut.getOrigin().getX() + ","+ reference.verticalCut.getOrigin().getY());;
-        System.out.println("Destination Coupe Vertical :" + reference.verticalCut.getDestination().getX() + ","+ reference.verticalCut.getDestination().getY());
-
-        switch (type) {
-            case INNER_TOP_RIGHT:
-                // Horizontal is to the left, vertical is below
-                distanceX = intersection.getX() - reference.horizontalCut.getOrigin().getX();
-                distanceY = reference.verticalCut.getOrigin().getY() - intersection.getY();
-                break;
-
-            case INNER_TOP_LEFT:
-                // Horizontal is to the right, vertical is below
-                distanceX = reference.horizontalCut.getOrigin().getX() - intersection.getX();
-                distanceY = reference.verticalCut.getOrigin().getY() - intersection.getY();
-                break;
-
-            case INNER_BOTTOM_LEFT:
-                // Horizontal is above, vertical is to the right
-                distanceX = reference.horizontalCut.getOrigin().getX() - intersection.getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            case INNER_BOTTOM_RIGHT:
-                // Horizontal is above, vertical is to the left
-                distanceX = intersection.getX() - reference.horizontalCut.getOrigin().getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            case OUTER_TOP_RIGHT:
-                // Horizontal is to the left, vertical is above
-                distanceX = intersection.getX() - reference.horizontalCut.getOrigin().getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            case OUTER_TOP_LEFT:
-                // Horizontal is to the right, vertical is above
-                distanceX = reference.horizontalCut.getOrigin().getX() - intersection.getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            case OUTER_BOTTOM_LEFT:
-                // Horizontal is below, vertical is to the right
-                distanceX = reference.horizontalCut.getOrigin().getX() - intersection.getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            case OUTER_BOTTOM_RIGHT:
-                // Horizontal is below, vertical is to the left
-                distanceX = intersection.getX() - reference.horizontalCut.getOrigin().getX();
-                distanceY = intersection.getY() - reference.verticalCut.getOrigin().getY();
-                break;
-
-            default:
-                throw new AssertionError();
-        }
-
-        this.horizontalCut = new ParallelCut(depth, tool, reference.horizontalCut, distanceX, intersection);
-        this.verticalCut = new ParallelCut(depth, tool, reference.verticalCut, distanceY, intersection);
-
-        /*System.out.println("Origine Coupe Horizontal :" + this.horizontalCut.getOrigin().getX() + ","+ this.horizontalCut.getOrigin().getY());;
-        System.out.println("Destination Coupe Horizontal :" + this.horizontalCut.getDestination().getX() + ","+ this.horizontalCut.getDestination().getY());;
-
-        System.out.println("Origine Coupe Vertical :" + this.verticalCut.getOrigin().getX() + ","+ this.verticalCut.getOrigin().getY());;
-        System.out.println("Destination Coupe Vertical :" + this.verticalCut.getDestination().getX() + ","+ this.verticalCut.getDestination().getY());;*/
+        this.horizontalCut = new StraightCUt(depth, tool, reference.horizontalCut, new Coordinate(reference.getX(), reference.getY()),intersection);
+        this.verticalCut = new StraightCUt(depth, tool, reference.verticalCut, new Coordinate(reference.getX(), reference.getY()),intersection);
     }
 
-    public ParallelCut getHorizontalCut() {
+    public StraightCUt getHorizontalCut() {
         return horizontalCut;
     }
 
-    public ParallelCut getVerticalCut() {
+    public StraightCUt getVerticalCut() {
         return verticalCut;
     }
 }
--- equipe45/src/main/java/Equipe45/domain/ParallelCut.java
+++ equipe45/src/main/java/Equipe45/domain/ParallelCut.java
@@ -5,6 +5,7 @@
 package Equipe45.domain;
 
 import Equipe45.domain.Utils.Coordinate;
+import Equipe45.domain.Utils.ReferenceCoordinate;
 
 /**
  *
@@ -13,42 +14,19 @@ import Equipe45.domain.Utils.Coordinate;
 public class ParallelCut extends RegularCut {
     private RegularCut referenceCut;
     private float distance;
+    private Coordinate referenceCoordinate;
     private Coordinate intersection;
 
     public ParallelCut(float depth, Tool tool, RegularCut referenceCut, float distance) {
-        super(depth, tool, calculateOrigin(referenceCut, distance, null), calculateDestination(referenceCut, distance, null));
+        super(depth, tool, calculateOrigin(referenceCut, distance, null,null), calculateDestination(referenceCut, distance, null ,null));
         this.referenceCut = referenceCut;
         this.distance = distance;
+        this.referenceCut = null;
         this.intersection = null;
     }
 
-    public ParallelCut(float depth, Tool tool, RegularCut referenceCut, float distance, Coordinate intersection) {
-        super(depth, tool, calculateOrigin(referenceCut, distance, intersection), calculateDestination(referenceCut, distance, intersection));
-        this.referenceCut = referenceCut;
-        this.distance = distance;
-        this.intersection = intersection;
-    }
 
-    private static Coordinate calculateOrigin(RegularCut referenceCut, float distance, Coordinate intersection) {
-        if (intersection != null) {
-            return calculateOriginWithIntersection(referenceCut, distance, intersection);
-        }
-       else {
-            return calculateOriginWithoutIntersection(referenceCut, distance);
-         }
-    }
-
-
-    private static Coordinate calculateDestination(RegularCut referenceCut, float distance, Coordinate intersection) {
-        if (intersection != null) {
-            return calculateDestinationWithIntersection(referenceCut, distance, intersection);
-        }
-        else {
-            return calculateDestinationWithoutIntersection(referenceCut, distance);
-        }
-    }
-
-    private static Coordinate calculateOriginWithoutIntersection(RegularCut referenceCut, float distance) {
+    private static Coordinate calculateOrigin(RegularCut referenceCut, float distance, Coordinate referenceCoordinate, Coordinate intersection) {
         Coordinate origin = referenceCut.getOrigin();
         if (referenceCut.isVertical()) {
             return new Coordinate(origin.getX() + distance, origin.getY());
@@ -59,35 +37,13 @@ public class ParallelCut extends RegularCut {
         }
     }
 
-    private static Coordinate calculateDestinationWithoutIntersection(RegularCut referenceCut, float distance) {
+
+    private static Coordinate calculateDestination(RegularCut referenceCut, float distance, Coordinate referenceCoordinate, Coordinate intersection) {
         Coordinate destination = referenceCut.getDestination();
         if (referenceCut.isVertical()) {
             return new Coordinate(destination.getX() + distance,  destination.getY());
         } else if (referenceCut.isHorizontal()) {
             return new Coordinate(destination.getX(), destination.getY() + distance);
-        } else {
-            throw new IllegalArgumentException("Reference cut is neither vertical nor horizontal");
-        }
-    }
-
-
-    private static Coordinate calculateOriginWithIntersection(RegularCut referenceCut, float distance, Coordinate intersection) {
-        Coordinate origin = referenceCut.getOrigin();
-        if (referenceCut.isVertical()) {
-            return new Coordinate(intersection.getX(), origin.getY());
-        } else if (referenceCut.isHorizontal()) {
-            return new Coordinate(origin.getX(), intersection.getY());
-        } else {
-            throw new IllegalArgumentException("Reference cut is neither vertical nor horizontal");
-        }
-    }
-
-    private static Coordinate calculateDestinationWithIntersection(RegularCut referenceCut, float distance, Coordinate intersection) {
-        Coordinate destination = referenceCut.getDestination();
-        if (referenceCut.isVertical()) {
-            return new Coordinate(intersection.getX(),  intersection.getY());
-        } else if (referenceCut.isHorizontal()) {
-            return new Coordinate(intersection.getX(), intersection.getY());
         } else {
             throw new IllegalArgumentException("Reference cut is neither vertical nor horizontal");
         }
